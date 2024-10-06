cb52313d75f0ce18297c9a15d0fe2ca8cbd97461
AgustinG130 <cafedeagustin@gmail.com>
2024-10-06 17:13:34 -0300

    fix

--- src/componentes/Information/information.jsx
+++ src/componentes/Information/information.jsx
@@ -8,7 +8,16 @@ export default function InformationPage() {
             INFORMATION
           </h1>
           <div className="mb-8">
-            <h2 className = "information-second-title" >What is Hydrosphere</h2>
+            <h2 className = "information-second-title" >What are GLOBE protocols</h2>
+            <p className="text-lg text-gray-700 leading-relaxed">
+            GLOBE protocols are procedures for collecting scientific environmental data, organized by Earth's spheres (atmosphere, biosphere, hydrosphere, pedosphere).
+            Part of the NASA-sponsored GLOBE program, they promote global science education and research. 
+            Protocols cover topics like air temperature, water pH, and soil moisture, using specific instruments and adhering to GLOBE standards.
+            Measurement frequency varies, with some needing daily input, while others are site-specific.
+            </p>
+          </div>
+          <div className="mb-8">
+            <h3 className = "information-second-title" >What is Hydrosphere</h3>
             <p className="text-lg text-gray-700 leading-relaxed">
             It's when we talk about the Earth's surface waters, including streams, rivers, lakes and coastal waters.
             GLOBE's research in the hydrosphere allows us to monitor these waters to understand their conditions,
@@ -16,7 +25,7 @@ export default function InformationPage() {
             </p>
           </div>
           <div className="mb-8">
-            <h2 className = "information-third-title" >What is pH</h2>
+            <h4 className = "information-second-title" >What is pH</h4>
             <p className="text-lg text-gray-700 leading-relaxed">
             The pH is a measure that indicates the level of acidity or alkalinity of a solution, based on the concentration of hydrogen ions present.
             The pH scale ranges from 0 to 14, with 7 being neutral. Solutions with a pH less than 7 are considered acidic,
